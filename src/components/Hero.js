39de375e0a3fdb0f17e74b9b783e1d3059f79277
Justin_Phillips <dcoltwinchester1967@gmail.com>
2022-03-16 12:17:41 -0400

    finished editing layout

---
+++ src/components/Hero.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Hero.css';
+import Crypto from '../assets/hero-img.png';
+
+function Hero() {
+    return (
+        <div className='hero'>
+            <div className='container'>
+                {/* Left Side */}
+                <div className='left'>
+                    <p>Buy & Sell Crypto 24/7 using your retirement account</p>
+                    <h1>Invesrt in Cryptocurrency with Your IRA</h1>
+                    <p>Buy, Sell, and Store hundreds of cryptocurrencies</p>
+                    <div className='input-container'>
+                        <input type='email' placeholder='Enter your email' />
+                        <button className='btn'>Learn More</button>
+                    </div>
+                </div>
+
+                {/* Right Side*/}
+                <div className='right'>
+                    <div className='img-container'>
+                        <img src={Crypto} alt='' />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Hero;
